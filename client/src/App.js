2046bd081816b355a0f03ceed28491d8418bef86
Ryohei Nagasawa <RyoheiNagasawa@MacBook-Pro.local>
2020-10-09 21:48:59 +0900

    ADD: calculatePrice #4

--- client/src/App.js
+++ client/src/App.js
@@ -12,45 +12,47 @@ import axios from 'axios';
 let currentId = 0;
 
 class App extends React.Component {
-    constructor(props) {
-      super(props)
+  constructor(props) {
+    super(props)
 
-      this.state = {
-        wants: [],
-      };
-    }
+    this.state = {
+      wants: [],
+      totalPrice: 0,
+    };
+  }
   render() {
-    return(
+    return (
       <div>
         <h1>欲しいものリスト</h1>
-          <Form onSubmit={this.handleSubmit}/>
-            {this.state.wants.map(({ id, goodsName, url, price, img, editing,index}) => (
-              <li key={id}>
-                {editing ? (
-                  <EditWant
-                    id={id}
-                    goodsName={goodsName}
-                    url={url}
-                    price={price}
-                    img={img}
-                    onCancel={this.handleChangeWantAttribute}
-                    onSubmit={this.handleUpdateWantText}
-                  />
-                ): (
-                  <Want
-                    key={id}
-                    id={id}
-                    goodsName={goodsName}
-                    url={url}
-                    price={price}
-                    img={img}
-                    onChange={this.handleChangeWantAttribute}
-                    onDelete={this.handleClickDelete}
-                    index={index}
-                  />
-                )}
-              </li>
-            ))}
+        <p>合計金額：{this.state.totalPrice}</p>
+        <Form onSubmit={this.handleSubmit} />
+        {this.state.wants.map(({ id, goodsName, url, price, img, editing, index }) => (
+          <li key={id}>
+            {editing ? (
+              <EditWant
+                id={id}
+                goodsName={goodsName}
+                url={url}
+                price={price}
+                img={img}
+                onCancel={this.handleChangeWantAttribute}
+                onSubmit={this.handleUpdateWantText}
+              />
+            ) : (
+                <Want
+                  key={id}
+                  id={id}
+                  goodsName={goodsName}
+                  url={url}
+                  price={price}
+                  img={img}
+                  onChange={this.handleChangeWantAttribute}
+                  onDelete={this.handleClickDelete}
+                  index={index}
+                />
+              )}
+          </li>
+        ))}
       </div>
     );
   }
@@ -58,61 +60,67 @@ class App extends React.Component {
   handleSubmit = e => {
     const newWant = {
       id: currentId,
-      goodsName:e.goodsName,
-      url:e.url,
-      price:e.price,
-      img:e.img,
+      goodsName: e.goodsName,
+      url: e.url,
+      price: e.price,
+      img: e.img,
       editing: false,
     };
     axios.post('/api/lists', {
       newWant
     }) // オブジェクトをサーバーにPOST
-    .then(response => {
-      console.log(response) // 後で行う動作確認のためのコンソール出力
-    })
-    .catch(err => {
-      console.error(new Error(err))
-    })
+      .then(response => {
+        console.log(response) // 後で行う動作確認のためのコンソール出力
+      })
+      .catch(err => {
+        console.error(new Error(err))
+      })
     const newWants = [...this.state.wants, newWant]
     this.setState({ wants: newWants })
+    this.calculatePrice(Number(e.price))
     currentId++;
   };
 
-  handleUpdateWantText = (id, e ) => {
+  handleUpdateWantText = (id, e) => {
     const newWant = this.state.wants.map(want => {
       if (want.id === id) {
-      return {
-        ...want,
-        goodsName:e.goodsName,
-        url:e.url,
-        img:e.img,
-        editing:false,
-      };
-    }
+        return {
+          ...want,
+          goodsName: e.goodsName,
+          url: e.url,
+          img: e.img,
+          editing: false,
+        };
+      }
 
-    return want
+      return want
     });
 
-    this.setState({wants:newWant});
+    this.setState({ wants: newWant });
   };
 
   handleClickDelete = id => {
-    const newWant = this.state.wants.filter(want => want.id !==id)
-    this.setState({ wants:newWant })
+    const newWant = this.state.wants.filter(want => want.id !== id)
+    this.setState({ wants: newWant })
   };
 
   handleChangeWantAttribute = (id, key, value) => {
     const newWant = this.state.wants.map(want => {
-      if (want.id===id) {
+      if (want.id === id) {
         return {
           ...want,
-          [key] : value,
+          [key]: value,
         };
       }
       return want;
     })
-    this.setState({ wants:newWant })
+    this.setState({ wants: newWant })
   };
+
+  calculatePrice = (price) => {
+    price = this.state.totalPrice + price
+    this.setState({ totalPrice: price })
+  }
 }
 
 export default App;
