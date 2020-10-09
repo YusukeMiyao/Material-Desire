51a24394b09d7a3a344758b249df53d81e08d4a1
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2020-10-09 15:46:10 +0900

    default-dir add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Form from './Form';
+import Want from './Want';
+import EditWant from './EditWant';
+// import styled from 'styled-components';
+// import '@atlaskit/css-reset';
+
+
+let currentId = 0;
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        wants: [],
+      };
+    }
+  render() {
+    return(
+      <div>
+        <h1>欲しいものリスト</h1>
+          <Form onSubmit={this.handleSubmit}/>
+            {this.state.wants.map(({ id, goodsName, url, price, img, editing,index}) => (
+              <li key={id}>
+                {editing ? (
+                  <EditWant 
+                    id={id}
+                    goodsName={goodsName}
+                    url={url}
+                    price={price}
+                    img={img}
+                    onCancel={this.handleChangeWantAttribute}
+                    onSubmit={this.handleUpdateWantText}
+                  />
+                ): (
+                  <Want 
+                    key={id}
+                    id={id}
+                    goodsName={goodsName}
+                    url={url}
+                    price={price}
+                    img={img}
+                    onChange={this.handleChangeWantAttribute}
+                    onDelete={this.handleClickDelete}
+                    index={index}
+                  />
+                )} 
+              </li>
+            ))}
+      </div>
+    );
+  }
+  
+  handleSubmit = e => {
+    const newWant = {
+      id: currentId,
+      goodsName:e.goodsName,
+      url:e.url,
+      price:e.price,
+      img:e.img,
+      editing: false,
+    };
+    const  newWants = [...this.state.wants, newWant]
+    this.setState({ wants: newWants })
+    currentId++;
+  };
+
+  handleUpdateWantText = (id, e ) => {
+    const newWant = this.state.wants.map(want => {
+      if (want.id === id) {
+      return {
+        ...want,
+        goodsName:e.goodsName,
+        url:e.url,
+        img:e.img,
+        editing:false,
+      };
+    }
+
+    return want
+    });
+
+    this.setState({wants:newWant});
+  };
+
+  handleClickDelete = id => {
+    const newWant = this.state.wants.filter(want => want.id !==id)
+    this.setState({ wants:newWant })
+  };
+
+  handleChangeWantAttribute = (id, key, value) => {
+    const newWant = this.state.wants.map(want => {
+      if (want.id===id) {
+        return {
+          ...want,
+          [key] : value,
+        };
+      }
+      return want;
+    })
+    this.setState({ wants:newWant })
+  };
 }
 
 export default App;
---
+++ src/Want.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+// const Container = styled.div`
+//   display:flex;
+// `
+
+class Want extends React.Component {
+  render() {
+    const { goodsName, url, img, price} = this.props
+        
+    return(
+      <div>
+        <p>欲しいもの:{goodsName}</p>
+        <p>URL・場所:{url}</p>
+        <p>値段:{price}</p>
+        <img src={img} height={100} width={100}/>
+        <button onClick={this.handleClickEdit}>編集</button>
+        <button onClick={this.handoleClickDelete}>削除</button>
+      </div>
+    );
+  }
+
+      handleClickEdit = () => {
+        const { onChange, id, editing }  = this.props;
+        onChange(id, 'editing', !editing);
+      };
+
+      handoleClickDelete = () => {
+        const { onDelete, id } = this.props;
+        onDelete(id);
+      };
+}
+
+export default Want;
