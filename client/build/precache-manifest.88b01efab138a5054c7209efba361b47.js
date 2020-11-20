a9e4c181e62ff5c9a3bc46f6d9fef94e29a1a6c6
Ryohei Nagasawa <RyoheiNagasawa@MacBook-Pro.local>
2020-11-20 18:59:52 +0900

    ADD: firebase deploy #45

---
+++ client/build/precache-manifest.88b01efab138a5054c7209efba361b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5871677e0ae767a832a7aad7b0b2cedb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29302f4c137d39805892",
+    "url": "/static/css/main.f22f31f9.chunk.css"
+  },
+  {
+    "revision": "51ed7d9582f8a970730e",
+    "url": "/static/js/2.08f2f3d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08f2f3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29302f4c137d39805892",
+    "url": "/static/js/main.738a4558.chunk.js"
+  },
+  {
+    "revision": "fec04657fcf43e526cd7",
+    "url": "/static/js/runtime-main.d69db0fc.js"
+  },
+  {
+    "revision": "f6ae3dbc5bbf4f1f2bfe8e323172aab8",
+    "url": "/static/media/Icon.f6ae3dbc.png"
+  }
+]);
